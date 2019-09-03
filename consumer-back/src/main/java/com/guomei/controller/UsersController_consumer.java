d66fd8415b3fdac9c16fe5eb87fb26b5fea915c4
hmn312 <1902732695@qq.com>
2019-09-03 17:19:21 +0800

    管理员登录后进入后台管理

--- consumer-back/src/main/java/com/guomei/controller/UsersController_consumer.java
+++ consumer-back/src/main/java/com/guomei/controller/UsersController_consumer.java
@@ -1,14 +1,35 @@
 package com.guomei.controller;
 
-import com.guomei.service.UsersService;
+import com.guomei.pojo.Users;
+import com.guomei.service.UsersService_consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/Users")
 @Controller
 public class UsersController_consumer {
 
     @Autowired
-    private UsersService usersService;
+    private UsersService_consumer usersService_consumer;
+
+    //管理员后台登录
+    @RequestMapping("/Back/adminLogin")
+    public String adminLogin(Users users, Model model) {
+        //解密
+        //String password = DigestUtils.md5DigestAsHex(users.getPassWord().getBytes());
+       // users.setPassWord(password);
+        if (usersService_consumer.adminLogin(users) == null) {
+            model.addAttribute("errorMess", "用户名或密码错误");
+            return "login";
+        } else {
+            return "frame";
+        }
+    }
+
+
+
 }
--- consumer-back/src/main/java/com/guomei/service/UsersService_consumer.java
+++ consumer-back/src/main/java/com/guomei/service/UsersService_consumer.java
@@ -5,11 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @FeignClient("users-server")
-public interface UsersService {
+public interface UsersService_consumer {
 
     @RequestMapping("/adminLogin")
      Users adminLogin(Users users);
 
-    
+
 }
