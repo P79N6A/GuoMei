c6b35d777c65fa935f0baebedd66096c3c5d9fc1
Lvxiing <1242713963@qq.com>
2019-09-03 22:19:21 +0800

    上传前台页面

--- consumer-back/src/main/java/com/guomei/controller/OrdersController_consumer.java
+++ consumer-back/src/main/java/com/guomei/controller/OrdersController_consumer.java
@@ -16,6 +16,82 @@ public class OrdersController_consumer {
     @ResponseBody
     @RequestMapping("test")
     public String test(){
-        return null;
+        String json = "{\n" +
+                "  \"data\": [\n" +
+                "    {\n" +
+                "      \"categoryId\": 1,\n" +
+                "      \"categoryName\": \"分类\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": -1\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 2,\n" +
+                "      \"categoryName\": \"用户管理\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 1\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 3,\n" +
+                "      \"categoryName\": \"查询用户\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 2\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 4,\n" +
+                "      \"categoryName\": \"添加用户\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 2\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 5,\n" +
+                "      \"categoryName\": \"修改用户\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 2\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 6,\n" +
+                "      \"categoryName\": \"删除用户\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 2\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 7,\n" +
+                "      \"categoryName\": \"角色管理\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 1\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 8,\n" +
+                "      \"categoryName\": \"查询角色\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 7\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 9,\n" +
+                "      \"categoryName\": \"添加角色\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 7\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 10,\n" +
+                "      \"categoryName\": \"修改角色\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 7\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 11,\n" +
+                "      \"categoryName\": \"删除角色\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 7\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"categoryId\": 12,\n" +
+                "      \"categoryName\": \"角色权限管理\",\n" +
+                "      \"menuUrl\": 123,\n" +
+                "      \"parentId\": 7\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        return json;
     }
 }
