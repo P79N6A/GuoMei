abfa046f8667d052a8e25daae1e2df76804db865
LogoLiuJun <2650702166@qq.com>
2019-09-05 08:44:11 +0800

    后台订单更新方法

--- order-server/src/main/java/com/guomei/controller/OrdersController.java
+++ order-server/src/main/java/com/guomei/controller/OrdersController.java
@@ -3,7 +3,13 @@ package com.guomei.controller;
 import com.guomei.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/Back/Orders")
 @Controller
@@ -11,4 +17,23 @@ public class OrdersController {
 
     @Autowired
     private OrdersService ordersService;
+
+    //查询所有订单详情
+    @RequestMapping("/orderList/{orderNo}/{name}")
+    @ResponseBody
+    public List<Map<String,Object>> orderList(@PathVariable("orderNo")String orderNo, @PathVariable("name")String name){
+        Map<String,Object>map=new HashMap<>();
+        map.put("orderNo",orderNo);
+        map.put("name",name);
+        return ordersService.orderList(map);
+    }
+
+    //查询退货单,退货金额,金额合计,订货单 ,订货金额
+    public Map<String,Object> inquiryAmount(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("orderQuantity",ordersService.orderQuantity());
+        map.put("returnQuantity",ordersService.returnQuantity());
+        map.put("orderAmount",ordersService.orderAmount());
+        return map;
+    }
 }
--- order-server/src/main/java/com/guomei/dao/OrdersDao.java
+++ order-server/src/main/java/com/guomei/dao/OrdersDao.java
@@ -2,6 +2,17 @@ package com.guomei.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface OrdersDao {
+    //查询所有订单详情
+    public List<Map<String,Object>> orderList(Map<String,Object>map);
+    //订货单  订货金额
+    public List<Map<String,Object>> orderQuantity();
+    //退货单 退货金额
+    public List<Map<String,Object>> returnQuantity ();
+    //金额合计
+    public double orderAmount();
 }
