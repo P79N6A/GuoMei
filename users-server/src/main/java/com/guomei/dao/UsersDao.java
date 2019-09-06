12374a9f91d439ca02160275f5e071df88581572
hmn312 <1902732695@qq.com>
2019-09-06 16:17:23 +0800

    删除用户.修改用户

--- users-server/src/main/java/com/guomei/dao/UsersDao.java
+++ users-server/src/main/java/com/guomei/dao/UsersDao.java
@@ -17,5 +17,14 @@ public interface UsersDao {
     //查询用户
     List<Users> findUsers(@Param("userName") String userName);
 
+    //删除用户
+   int  delUser(Integer id);
+
+   //根据id查询用户信息
+    Users   findById(Integer id);
+
+    //修改用户
+   int updateUser(Users user);
+
 
 }
